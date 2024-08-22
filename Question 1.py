4e1bf813ec7be7793dc1940a49b30c1c85d14356
bishresearch <biswajits.info@gmail.com>
2024-08-22 13:26:53 +0600

    s

---
+++ Question 1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a value for a:" )) 
+b = int(input("Enter a value for b:")) 
+
+print("Swap not inetiated yet")
+print("a is", a, "and", "b is", b)
+
+a, b = b, a
+
+print("After Swap")
+print("a is", a, "and", "b is", b)
